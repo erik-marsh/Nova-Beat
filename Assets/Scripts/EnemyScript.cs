927e9f8f0138c4e1efbc3ff2321041a643a04aa9
Erik Marsh <erik.i.marsh@gmail.com>
2021-04-19 01:56:49 -0700

    main scene setup, lots of stuff

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+	public GameObject projectilePrefab;
+	public float spawnInterval = 2.0f;
+	private float timer = 0.0f;
+
+	private void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer >= spawnInterval)
+		{
+			var proj = Instantiate(projectilePrefab, new Vector3(8, 0, 0), Quaternion.identity);
+			proj.transform.parent = GameObject.Find("Projectiles").transform;
+			timer = 0.0f;
+		}
+	}
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
+	public Text hitText;
+	private int numHits = 0;
+
+	private void Update()
+	{
+		hitText.text = numHits.ToString();
+	}
+
 	private void OnTriggerEnter(Collider collision)
 	{
 		Debug.Log("Player Collided");
+		numHits++;
 		MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
 		meshRenderer.material.color = new Color(255, 0, 0);
 	}
