efcb6e262f145804ae042602063add3be416cb26
Erik Marsh <erik.i.marsh@gmail.com>
2021-04-23 01:08:13 -0700

    small changes

---
+++ Assets/Scripts/TestRhythmScript.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRhythmScript : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public GameObject PlayerObject;
+
+    private float timer = 0.0f;
+    private Song song;
+    private int firingPointIndex = 0;
+    private bool ship = false;
+
+	private void Start()
+	{
+        song = new Song(120, PlayerObject.transform.localPosition.x, transform.localPosition.x);
+
+        float beat = 0.0f;
+        float twoMeasureOffset = 0.0f;
+        for(int i = 0; i < 10; i++)
+        {
+            twoMeasureOffset = 8.0f;
+            song.AddFiringPoint(beat + 1.0f, twoMeasureOffset); // this way you can explicitly set which beat the projectile is shot on
+            song.AddFiringPoint(beat + 2.0f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 3.0f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 3.5f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 4.0f, twoMeasureOffset);
+
+            song.AddFiringPoint(beat + 5.0f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 5.5f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 6.0f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 6.5f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 7.0f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 7.5f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 8.0f, twoMeasureOffset);
+            song.AddFiringPoint(beat + 8.5f, twoMeasureOffset);
+
+            beat += 16.0f;
+		}
+	}
+
+	private void Update()
+	{
+        timer += Time.deltaTime;
+
+        if (firingPointIndex < song.firingPoints.Count && timer >= song.firingPoints[firingPointIndex].startTime)
+		{
+            projectilePrefab.GetComponent<Entity381>().velocity.x = song.firingPoints[firingPointIndex].velocity;
+            if (ship)
+            {
+                Instantiate(projectilePrefab, transform.localPosition, Quaternion.identity);
+            }
+            else
+			{
+                Vector3 t = transform.localPosition;
+                t.y += 3;
+                Instantiate(projectilePrefab, t, Quaternion.identity);
+
+            }
+
+            firingPointIndex++;
+            ship = !ship;
+        }
+	}
+}
+
+public class Song
+{
+    public int bpm;
+    public List<FiringPoint> firingPoints;
+
+    private float beatLength;
+    private float playerEnemyDistance;
+
+    public Song(int bpm, float playerPosition, float enemyPosition)
+    {
+        this.bpm = bpm;
+        this.beatLength = 60.0f / this.bpm;
+        this.playerEnemyDistance = enemyPosition - playerPosition; // WARNING: sign matters here
+
+        firingPoints = new List<FiringPoint>();
+    }
+
+    public void AddFiringPoint(float fireBeat, float playerBeatOffset)
+    {
+        float playerBeat = fireBeat + playerBeatOffset;
+        fireBeat -= 1.0f; // convert to 0 index
+        playerBeat -= 1.0f;
+        float diff = playerBeat - fireBeat;
+        float firingTime = fireBeat * beatLength;
+        float projectileSpeed = playerEnemyDistance / (diff * beatLength);
+        firingPoints.Add(new FiringPoint(firingTime, -projectileSpeed));
+    }
+}
+
+public struct FiringPoint
+{
+    public float startTime;
+    public float velocity;
+
+    public FiringPoint(float startTime, float velocity)
+	{
+        this.startTime = startTime;
+        this.velocity = velocity;
+	}
+}
